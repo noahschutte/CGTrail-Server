e4cec75e0e97e3b329a7a080b6e4ed779a097194
noahschutte <noahschutte@gmail.com>
2018-08-11 17:09:24 -0600

    plural route names

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ let mongoConnection
 const BusinessesRepository = require('./BusinessesRepository')
 let businessesRepository
 
-const User = require('./models/User')
+const User = require('./models/user')
 var { authenticate } = require('./middleware/authenticate');
 
 const port = process.env.PORT || 8000;
@@ -59,7 +59,7 @@ app.get('/businesses', async (req, res) => {
     }
 });
 
-app.get('/business/:id', async (req, res) => {
+app.get('/businesses/:id', async (req, res) => {
     const businessId = req.params.id
     if (typeof businessId != "string") {
         const errorMessage = `Bad request. Business _id must be a string.`
@@ -104,7 +104,7 @@ app.post('/users/login', (req, res) => {
     });
 });
 
-app.delete('/users/me/token', authenticate, (req, res) => {
+app.delete('/users/logout', authenticate, (req, res) => {
     req.user.removeToken(req.token).then(() => {
         res.status(200).send();
     }, () => {
@@ -116,11 +116,11 @@ app.post('/businesses', authenticate, async (req, res) => {
 
 })
 
-app.put('business/:id', authenticate, async (req, res) => {
+app.put('businesses/:id', authenticate, async (req, res) => {
 
 })
 
-app.delete('business/:id', authenticate, async (req, res) => {
+app.delete('businesses/:id', authenticate, async (req, res) => {
 
 })
 
