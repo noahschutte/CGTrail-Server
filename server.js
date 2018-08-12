14878180cd53f876fa92f646ecb8e88bc547aa0d
noahschutte <noahschutte@gmail.com>
2018-08-11 19:37:38 -0600

    include body-parser

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 const cors = require('cors');
 
 const Log = require('log');
